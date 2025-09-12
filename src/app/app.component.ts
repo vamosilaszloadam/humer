c86f1080e4c52c8b84adbe6b6dbdeab25d304d94
Vámosi László Ádám <vamosilaszloadam@outlook.com>
2025-09-12 10:45:14 +0200

    Navigáció kész

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 // import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    // LoginComponent
-  ],
+    RouterOutlet,
+    RouterLink
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
