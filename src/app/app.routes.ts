142b5b08040c8a9cd8bfe32137381250eca21b8a
Vámosi László Ádám <vamosilaszloadam@outlook.com>
2025-09-12 10:31:04 +0200

    Komponensek létrehozva. Routing kész

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { RankComponent } from './rank/rank.component';
+import { ProjectComponent } from './project/project.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'logout', component: LogoutComponent },
+    { path: 'employee', component: EmployeeComponent },
+    { path: 'rank', component: RankComponent },
+    { path: 'project', component: ProjectComponent },
+    { path: 'user', component: UserComponent }
+];
---
+++ src/app/rank/rank.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rank',
+  standalone: true,
+  imports: [],
+  templateUrl: './rank.component.html',
+  styleUrl: './rank.component.css'
+})
+export class RankComponent {
+
+}
