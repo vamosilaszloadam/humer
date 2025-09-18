a5cdd53931302793d6dce964668aff11798ffb6c
Vámosi László Ádám <vamosilaszloadam@outlook.com>
2025-09-18 10:14:34 +0200

    Az admin/user megoldva

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { EmployeeComponent } from './employee/employee.component';
 import { RankComponent } from './rank/rank.component';
 import { ProjectComponent } from './project/project.component';
 import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -15,5 +16,9 @@ export const routes: Routes = [
     { path: 'employee', component: EmployeeComponent },
     { path: 'rank', component: RankComponent },
     { path: 'project', component: ProjectComponent },
-    { path: 'user', component: UserComponent }
+    { path: 'admin', component: AdminComponent, 
+        children: [
+            { path: 'user', component: UserComponent }
+        ]
+    }
 ];
