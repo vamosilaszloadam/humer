83b345b183cfe007a5aba35390e0819345483ac3
Vámosi László Ádám <vamosilaszloadam@outlook.com>
2025-09-25 12:25:28 +0200

    Nap vége

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
 import { UserService } from '../shared/user.service';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
 
+  userForm: any;
   userList: any[] = []
-
-  constructor(private user: UserService) {}
+  passwordVisible = false
+  
+  constructor(
+    private user: UserService,
+    private builder: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.userForm = this.builder.group({
+      user: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      pass: ['', [Validators.required, Validators.minLength(3)]],
+      passre: '',
+      roleId: ''
+    })
     // this.userList = this.user.getUsers();
     this.user.getUsers().subscribe({
       next: (res: any) => {
@@ -30,4 +43,19 @@ export class UserComponent {
     })
     // console.log(this.userList)
   }
+  createUser(event: SubmitEvent) {
+    console.log('Létrehozás...')
+
+    console.log(this.userForm.value)
+
+    //Küldés REST API végpontra
+
+    event.preventDefault();
+  }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible
+  }
+
 }
+
